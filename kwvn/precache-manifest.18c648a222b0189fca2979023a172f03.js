5221439f4e774c1554ebf5f12239e2b5ef9ffe75
Hung Blue <theblue18@gmail.com>
2021-08-31 15:14:15 +0700

    update kwvn

---
+++ kwvn/precache-manifest.18c648a222b0189fca2979023a172f03.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651640e440d4ef6c35b6642a35466e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18bb9dc3bafc00455c1",
+    "url": "/static/css/main.281423d6.chunk.css"
+  },
+  {
+    "revision": "d4ddddc8599850172821",
+    "url": "/static/js/2.1a0dfa6e.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/static/js/2.1a0dfa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18bb9dc3bafc00455c1",
+    "url": "/static/js/main.af7978c8.chunk.js"
+  },
+  {
+    "revision": "718b162186fb690ea25e",
+    "url": "/static/js/runtime-main.f95d4617.js"
+  },
+  {
+    "revision": "f3a224b78523e1df5e95f2002953f394",
+    "url": "/static/media/Agent_Comand_Transactions.f3a224b7.png"
+  },
+  {
+    "revision": "011b65be190252d0a4b32ef2eed40ecd",
+    "url": "/static/media/Agent_Command_Database.011b65be.png"
+  },
+  {
+    "revision": "8d8557fd202b908e27cab091c122321d",
+    "url": "/static/media/Agent_Command_Day.8d8557fd.png"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "a99d89f629f663e228e4045bb395da2e",
+    "url": "/static/media/logo_KW_Vietnam_Round.a99d89f6.png"
+  },
+  {
+    "revision": "ea35f681657eb52751afa80097625996",
+    "url": "/static/media/logo_KW_Vietnam_Transparent.ea35f681.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
